1a111a52d60083eedad406892d28d35d52b1ccbe
jkim810 <jkim0810@gmail.com>
2018-12-08 15:40:16 -0500

    u

--- DB/chat.py
+++ DB/chat.py
@@ -8,13 +8,5 @@ class Chats(Document):
 	date = DateTimeField()#required=True)
 	message = StringField(required=True)
 
-class Chat:
-	def __init__(self, user, chatroom, date, text):
-		self.user = user
-		# user should be foreign key referencing a user // it should insert user objectid
-		self.chatroom = chatroom
-		# chatroom should be foreign key referencing a chatroom // it should insert chatroom objectid
-		self.date = date
-		# make sure to set date as date format
-		self.text = text
-		# make sure to set text as a text
+def chat(user, chatroom, date, text):
+
--- DB/chatroom.py
+++ DB/chatroom.py
@@ -1,4 +1,5 @@
 from mongoengine import *
+import pymongo
 import user
 # hijb
 # db = mydatabase
@@ -9,30 +10,20 @@ class Chatrooms(Document):
 	channel = StringField(required=True)
 	users = ListField(ReferenceField(user.Users), required=True)
 
-class Chatroom:
-	def __init__(self, chatroomID, channel, users = ''):
-		self.chatroomID = chatroomID
-		self.channel = channel
-		# users should be a list of user object id
-		if users == '':
-			self.users = set()
-		else:
-			self.users = users
+def makeChatroom(self, users):
+	# make chatroom for multiple users
+	print('a')
 
-	def makeChatroom(self, users):
-		# make chatroom for multiple users
-		print('a')
-
-	def getChatroom(self, user):
-		# query chatroom with speicified user using find in collection
-		print('a')
+def getChatroom(self, user):
+	# query chatroom with speicified user using find in collection
+	print('a')
 
 
-	def getChats(self):
-		# query chats within chatroom and return tuple of chat list marked with (mine, chat) and (others, chat)
-		print('a')
+def getChats(self):
+	# query chats within chatroom and return tuple of chat list marked with (mine, chat) and (others, chat)
+	print('a')
 
 
-	def searchChat(self, keyword):
-		# return lists of chat that contain the keyword and few neighboring chats
-		print('a')
+def searchChat(self, keyword):
+	# return lists of chat that contain the keyword and few neighboring chats
+	print('a')
