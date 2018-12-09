b15669d55c7f1d08f4a7e9ee5f0c5c19a3c140da
jkim810 <jkim0810@gmail.com>
2018-12-09 16:18:44 -0500

    much faster injection speed

--- a.py
+++ a.py
@@ -9,5 +9,10 @@ from time import time
 
 
 u = user.myusers.find_one({'username':'stuNNed'})
-print(u['_id'])
+print('user')
+print(u)
+print('chatrooms')
 print(user.mychatrooms.find_one({'users':u['_id']}))
+print('chats')
+for x in user.mychats.find({'user':u['_id']}):
+	print(x)
--- injectchats.py
+++ injectchats.py
@@ -1,15 +1,38 @@
 from mongoengine import *
 from time import time
-from pymongo.errors import DuplicateKeyError
+from pymongo.errors import BulkWriteError
 import user
 import chat
 import chatroom
 import sys
 from time import time
+from pprint import pprint
+
+def insert_records(l,total):
+	records = []
+	batchtime = time()
+	begin = time()
+	for i, c in enumerate(l):
+		cr = user.mychatrooms.find_one({'chatroomID':c[0][0], 'channel':c[0][1]})
+		us = user.myusers.find_one({'username': c[1]})
+		records.append({'chatroom': cr['_id'], 'user': us['_id'], 'date': c[2], 'message': c[3]})
+		if i % 10000 == 0:
+			print('Finding record: %8.0f/%8.0f\t%.2f\t%.2f\t%.2f'%(i,len(l),time()-batchtime, time()-begin,time()-total))
+			batchtime = time()
+
+	try:
+		user.mychats.insert_many(records, ordered=False)
+		#user.myusers.insert_one({'username': us, 'pw': user.encryptPassword(us)})
+	except BulkWriteError as bwe:
+		pprint(bwe.details)
+		pass
+
+	print('Inserting record: %8.0f/%8.0f\t%.2f\t%.2f\t%.2f'%(i,len(l),time()-batchtime, time()-begin,time()-total))
 
 
 file = open(sys.argv[1], 'rt', encoding='UTF-8')
 file.readline()
+total = time()
 begin = time()
 
 i = 0
@@ -21,32 +44,11 @@ for line in file:
 	# (chatroomID, channel), from, date, message
 	chatToInsert.append([(l[0],l[1]),l[3],l[2],l[5]])
 	i += 1
-	if i % 1000000 == 0:
-		print('Reading line: %8.0f\t%.2f\t%.2f'%(i,time()-batchtime, time()-begin))
-		batchtime = time()
+	if i % 100000 == 0:
+		print('Reading line: %8.0f\t%.2f\t%.2f\t%.2f'%(i,time()-batchtime, time()-begin,time()-total))
+		insert_records(chatToInsert,total)
+		chatToInsert = []
 
-begin = time()
+print("Time elapsed in seconds: " + str(time()-total))
 
-for c, i in enumerate(chatToInsert):
-	cr = user.mychatrooms.find_one({'chatroomID':c[0][0], 'channel':c[0][1]})
-	us = user.myusers.find_one({'username': c[1]})
-	chatToInsert[i][0] = cr['_id']
-	chatToInsert[i][1] = us['_id']
-	if i % 1000 == 0:
-		print('Finding record: %8.0f/%8.0f\t%.2f\t%.2f'%(i,len(cr),time()-batchtime, time()-begin))
-		batchtime = time()
 
-begin=time()
-for c, i in enumerate(chatToInsert):
-	try:
-		user.mychats.insert_one({'chatroom': c[0], 'user': c[1], 'date': c[2], 'message': c[3]})
-		#user.myusers.insert_one({'username': us, 'pw': user.encryptPassword(us)})
-	except DuplicateKeyError:
-		print(DuplicateKeyError.error_document)
-		pass
-
-	if i % 1000 == 0:
-		print('Inserting record: %8.0f/%8.0f\t%.2f\t%.2f'%(i,len(toinsert),time()-batchtime, time()-begin))
-		batchtime = time()
-
-print("Time elapsed in seconds: " + str(time()-begin))
